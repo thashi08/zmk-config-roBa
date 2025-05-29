7098e7d2481a9cf5ed48f539bcafea3302022018
thashi08 <terekomu@gmail.com>
2025-05-30 01:21:16 +0900

    Update mouse_accel.c

--- config/src/mouse_accel.c
+++ config/src/mouse_accel.c
@@ -7,12 +7,12 @@
 static void adjust_mouse_speed(struct zmk_sensor_input_event *ev) {
     int16_t size = ABS(ev->data.relative.x) + ABS(ev->data.relative.y);
     float mul = 0.6f;
-    if      (size > 60) mul = 0.006f;
-    else if (size > 30) mul = 0.005f;
-    else if (size >  5) mul = 0.004f;
-    else if (size >  3) mul = 0.003f;
-    else if (size >  2) mul = 0.002f;
-    else if (size >  1) mul = 0.001;
+    if      (size > 100) mul = 0.0006f;
+    else if (size > 30) mul = 0.0005f;
+    else if (size >  5) mul = 0.0004f;
+    else if (size >  3) mul = 0.0003f;
+    else if (size >  2) mul = 0.0002f;
+    else if (size >  1) mul = 0.0001;
 
     ev->data.relative.x = CLAMP(ev->data.relative.x * mul, -127, 127);
     ev->data.relative.y = CLAMP(ev->data.relative.y * mul, -127, 127);
